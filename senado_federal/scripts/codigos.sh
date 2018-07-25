bfd38c1c33c848f2ca8e2057931c02a68a77ad7d
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2018-07-25 16:12:41 -0300

    updating codes and getcodigo

--- senado_federal/scripts/codigos.sh
+++ senado_federal/scripts/codigos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -s "http://www.senado.gov.br/transparencia/rh/servidores/nova_consulta.asp?fnome=&fvinculo=&fsituacao=&flotacao=&fcategoria=&fcargo=0&fsimbolo=&ffuncao=0&fadini=&fadfim=&fconsulta=ok&btnsubmit=Pesquisar" | python getCodigoSenado.py
+curl -s "http://www.senado.leg.br/transparencia/rh/servidores/nova_consulta.asp?fnome=&fvinculo=&fsituacao=&flotacao=&fcategoria=&fcargo=0&fsimbolo=&ffuncao=0&fadini=&fadfim=&fconsulta=ok&btnsubmit=Pesquisar" | python getCodigoSenado.py
--- senado_federal/scripts/getCodigoSenado.py
+++ senado_federal/scripts/getCodigoSenado.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import sys
+import io
 
 def getCodigo(href):
 	return href.split('detalhe.asp?fcodigo=')[1]
 
-raw_content = "".join(sys.stdin.readlines())
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+
+raw_content = "".join(input_stream)
 soup = BeautifulSoup(raw_content, 'html.parser')
 
 
-with open('../dados/codigos.txt', 'w') as codigos:
+with open('../dados/codigos.txt', 'w', encoding="utf8") as codigos:
 	tbody = soup.find('tbody')
 	for a in tbody.find_all('a', href=True):
 		codigos.write(getCodigo(a['href']))
